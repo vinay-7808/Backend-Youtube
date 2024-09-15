fbbe19bdd46cad384dbbf22da755cdcf0f86672f
vinay-7808 <vinay211113233@gmail.com>
2024-09-15 05:41:58 +0530

    add user and video models

---
+++ src/models/user.models.js
@@ -0,0 +1,89 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"; // generate tokens not normal readable conytains headers and data. It contains secret code to decrypt
+import bcrypt from "bcrypt" // bearer token, used to hash password for encryption and decryption
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true, // for searching optimisation
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //Cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+userSchema.pre("save", async function (next) { // execute code just before saving (password encrypt)
+    if(!this.isModified("password")) return next() // if password is created or modified then only change
+
+    this.password = bcrypt.hash(this.password, 10) //hash round
+    next();
+}) 
+
+userSchema.methods.isPasswordCorrect = async function (password) { // to check password is correct or not
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () { 
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () { 
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User", userSchema)
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,7 +5,7 @@ class ApiError extends Error {
         errors = [],
         stack = ""
     ){
-        super(message)
+        super(message) //This calls the parent class’s constructor (from the Error class) and passes the message parameter. This ensures the error message is set properly.
         this.statusCode = statusCode
         this.data = null
         this.message = message
@@ -21,3 +21,11 @@ class ApiError extends Error {
 }
 
 export {ApiError};
+/*
+This ApiError class is useful in scenarios where you need a 
+consistent way to handle errors in an API response. It captures 
+an error’s message, status code, detailed error information, and
+the stack trace for easier debugging. It can be used to throw 
+custom errors in API routes or middleware, making error handling
+more structured and informative.
+*/
